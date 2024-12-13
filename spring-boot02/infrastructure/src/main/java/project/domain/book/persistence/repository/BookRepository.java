8f71c58afb08b9cfa153458b5e5afed9110a919d
RyuJeaMin <kariel7673@gmail.com>
2024-12-13 11:12:52 +0900

    feat :: BookCusteomRepository

--- spring-boot02/infrastructure/src/main/java/project/domain/book/persistence/repository/BookRepository.java
+++ spring-boot02/infrastructure/src/main/java/project/domain/book/persistence/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package project.domain.book.persistence.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import project.domain.book.persistence.entity.BookEntity;
 
-
-public interface BookRepository extends CrudRepository<BookEntity,Long> {
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
 
 }
